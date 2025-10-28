3ab4339c811b49d3515e2068e5942709ee131ab3
Guillermina Fiore <guillefiore03@gmail.com>
2025-10-28 14:45:31 -0300

    Update window.h

--- include/window.h
+++ include/window.h
@@ -1,9 +1,9 @@
-#ifndef WINDOW_H
+#ifndef WINDOW_H /* evita incluir este header dos veces */
 #define WINDOW_H
 
-#include "common.h"
+#include "common.h" /* trae el enum win_t (tipo ventana) */
 
-/* Aplica ventana in-place sobre N muestras */
+/* Aplica una ventana de suavizado (wtype) sobre un bloque de muestras (x), de tamaño (N) */
 void window_apply(double *x, int N, win_t wtype);
 
-#endif /* WINDOW_H */
+#endif
